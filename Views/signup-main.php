ac5bfd69492da92706932a6e0df5dbe95a85edff
Mustafa Marzouk <40539669+TopVirusMinus@users.noreply.github.com>
2022-04-22 20:57:58 +0200

    Add files via upload

---
+++ Views/signup-main.php
@@ -0,0 +1,12 @@
+<input type="text" required name="uname" placeholder="Enter Username" id="">
+<br>
+<br>
+<input type="email" required name="email" placeholder="Enter Email Address" id="">
+<br>
+<br>
+<input type="password" required name="Password" placeholder="Enter Password" id="">
+<br>
+<br>
+<input type="password" required name="Confirm-Password" placeholder="Confirm Password" id="">
+<br>
+<br>
