a422dc5f1e8ad816dc81341eaaf72f3c36d9c1a2
TopVirusMinus <mwmma5000@gmail.com>
2022-04-23 16:48:49 +0200

    fixed register id bug

--- Functions/register.function.php
+++ Functions/register.function.php
@@ -2,7 +2,12 @@
     require_once "../Classes/Files.class.php";
     //appends the post request's elements in a string (record)
     function createUser($attribs){
-        $record = "";
+        foreach($attribs as $a){
+            echo $a."<br>";
+        }
+        $mainAttribs = new File("../Database/mainAttribs.txt", "~");
+        $users = new File("../Database/users.txt", "~");
+
         //hashes password using md5 hash
         $attribs['Password'] = md5($attribs['Password']);
         
@@ -11,23 +16,21 @@
         
         //removes confirm-password because we are not validating passwords in this project :p
         unset($attribs["Confirm-Password"]);
-        print_r($attribs);        
+        print_r($attribs);
         //sorts the record data to start with the main attributes
         $record = "";
-        $mainAttribs = new File("../Database/mainAttribs.txt", "~");
+    
         $myfile = fopen($mainAttribs->getDestination(), "r+") or die("Unable to open file!");
         $newline = fgets($myfile);
         $lineArr = explode($mainAttribs->getSeparator(),$newline);
         //creates a string of record sorted by main attribs
         foreach($lineArr as $a){
+            echo "<br>".$a."<br>";
             $record .= $attribs[$a]."~";
             unset($attribs[$a]);
         } 
 
         $record .= implode("~", $attribs);
-
-        $userFile = new File("../Database/users.txt", "~");
-        $userFile->addRecord($record);
-        //echo $record;
+        $users->addRecord($record);
     }
 ?>    
