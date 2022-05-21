e3304cc07afbdcfc33b791ba36f1861c4b9530ea
stevenhani10 <stevenhany96@gmail.com>
2022-05-21 13:53:39 +0200

    the control of paymethod

---
+++ Controllers/payMethod.control.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+
+    $title = "Pay Method";
+    require_once "../Views/header.php";
+
+    require_once $_SESSION["classLocation"];
+    $userObj = unserialize($_SESSION["userObject"]);
+    print_r($userObj);
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+
+
+    require_once "../Model/Classes/Files.class.php";
+    require_once "../Views/payMethodView.php";
+
+    $paymthds = new File("../Database/paymethod.txt");
+    $AllPayMethods2D = $paymthds->getAllRecords();
+
+    payMethodView($AllPayMethods2D);
+
+?>
